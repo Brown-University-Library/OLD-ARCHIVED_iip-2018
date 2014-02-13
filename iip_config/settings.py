db3432c216e1c67e4ae042f9e70b792b33c5671b
birkin <birkin.diana@gmail.com>
2014-02-12 19:14:24 -0500

    fixes setting.

--- iip_config/settings.py
+++ iip_config/settings.py
@@ -7,7 +7,7 @@ import os
 
 temp_DEBUG = unicode( os.environ.get(u'IIP_DEBUG', u'') )
 assert temp_DEBUG in [ u'True', u'' ], Exception( u'DEBUG private setting is, "%s"; must be either "True" or ""' % temp_DEBUG )
-DEBUG = bool( temp_DEBUG ),
+DEBUG = bool( temp_DEBUG )
 
 TEMPLATE_DEBUG = DEBUG
 
