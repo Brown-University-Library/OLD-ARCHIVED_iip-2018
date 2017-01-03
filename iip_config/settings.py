e180283b19bc2d26384e7fd80dc1183f263c8b61
birkin <birkin.diana@gmail.com>
2017-01-03 10:50:11 -0500

    enables admin.

--- iip_config/settings.py
+++ iip_config/settings.py
@@ -144,7 +144,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'iip_search_app',
