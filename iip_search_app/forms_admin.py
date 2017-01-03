f9eb8f73cef3b5ec262c01db2b4b134f69909633
birkin <birkin.diana@gmail.com>
2017-01-03 14:33:14 -0500

    adds fields to admin display.

--- iip_search_app/forms_admin.py
+++ iip_search_app/forms_admin.py
@@ -6,18 +6,10 @@ from .models import StaticPage
 from .widgets import AddAnotherWidgetWrapper
 
 
-# class AdminBiographyForm(forms.ModelForm):
-#     bio = forms.CharField(widget=AdminPagedownWidget())
-
-#     class Meta:
-#         model = Biography
-#         fields = ('name', 'trp_id', 'alternate_names', 'external_id', 'birth_date', 'death_date', 'roles', 'bio')
-
-
 
 class AdminStaticPageForm(forms.ModelForm):
-    content = forms.CharField(widget=AdminPagedownWidget())
+    content = forms.CharField( widget=AdminPagedownWidget() )
 
     class Meta:
         model = StaticPage
-        fields = ( 'title', 'content' )
+        fields = ( 'slug', 'title_header', 'title', 'content' )
